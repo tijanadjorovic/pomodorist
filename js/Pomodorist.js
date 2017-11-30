26ec1e858ef0bd8d0de4813aa5b117c1eb939963
tijanadjorovic <tijana89djorovic@gmail.com>
2017-11-30 21:12:33 +0100

    added basic html

--- js/Pomodorist.js
+++ js/Pomodorist.js
@@ -1,1 +1,16 @@
+function start() {
+//    var timeStart = new Date(); ??????
+    var m = timeStart.getMinutes();
+    var s = timeStart.getSeconds();
 
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(function(){ startTime() }, 500);
+    
+    
+    function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
