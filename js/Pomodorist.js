4760e81ecc2191061d968f0e7a2028820e7d43ad
tijanadjorovic <tijana89djorovic@gmail.com>
2017-12-02 15:11:05 +0100

    added html css js

--- js/Pomodorist.js
+++ js/Pomodorist.js
@@ -4,25 +4,25 @@ var sound = new Audio();
 sound.src = "sounds/openhat.wav";
 
 //-----start main timer-----
-function start(){ 
+function start() {
     var timer = document.getElementById("mytimer").innerHTML;
     var arr = timer.split(":");
     var min = arr[0];
     var sec = arr[1];
-    
-    if (sec == 0){
-        if(min == 0){
-             sound.play();
-        return;
-    }
+
+    if (sec == 0) {
+        if (min == 0) {
+            sound.play();
+            return;
+        }
         min--;
         sec = 60;
-        if(min < 10) min = "0" + min;
-         sec = 59;
-    }   else sec--;
-    if(sec < 10) sec = "0" + sec;
-        document.getElementById("mytimer").innerHTML = min + ":" + sec;
-        interval = setTimeout(start, 1000);
+        if (min < 10) min = "0" + min;
+        sec = 59;
+    } else sec--;
+    if (sec < 10) sec = "0" + sec;
+    document.getElementById("mytimer").innerHTML = min + ":" + sec;
+    interval = setTimeout(start, 1000);
 }
 
 //------stop main timer-----
@@ -32,32 +32,32 @@ function stop() {
 }
 
 //-----rest main timer-----
-function reset(){
+function reset() {
     clearInterval(interval);
     document.getElementById("mytimer").innerHTML = '25:00';
 }
- 
+
 
 //-----start break timer-----
-function start05(){ 
+function start05() {
     var timer = document.getElementById("timer05").innerHTML;
     var arr = timer.split(":");
     var min = arr[0];
     var sec = arr[1];
-        if (sec == 0){
-            if(min == 0){
-                 sound.play();
+    if (sec == 0) {
+        if (min == 0) {
+            sound.play();
             return;
-            }
-             min--;
-             sec = 60;
-            if(min < 10) min = "0" + min;
-             sec = 59;
-    }   else sec--;
-        if(sec < 10) sec = "0" + sec;
-    
-        document.getElementById("timer05").innerHTML = min + ":" + sec;
-        interval05 = setTimeout(start05, 1000);
+        }
+        min--;
+        sec = 60;
+        if (min < 10) min = "0" + min;
+        sec = 59;
+    } else sec--;
+    if (sec < 10) sec = "0" + sec;
+
+    document.getElementById("timer05").innerHTML = min + ":" + sec;
+    interval05 = setTimeout(start05, 1000);
 }
 
 //-----stop break timer-----
@@ -66,8 +66,7 @@ function stop05() {
     interval05 = undefined;
 }
 //-----reset break timer-----
-function reset05(){
+function reset05() {
     clearInterval(interval05);
     document.getElementById("timer05").innerHTML = "05:00";
 }
-          
